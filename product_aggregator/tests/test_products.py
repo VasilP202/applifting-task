131597b89ac9109b121188fc571eef7709f87487
Vasil Poposki <vasil.poposki@endevel.cz>
2023-04-25 12:10:43 +0200

    Tests

--- product_aggregator/tests/test_products.py
+++ product_aggregator/tests/test_products.py
@@ -1,17 +1,13 @@
 import json
-import uuid
-from datetime import datetime, timedelta
 
-import requests
 from django.test import TestCase
 from model_bakery import baker
 from rest_framework.test import APIClient
 
-from api.use_cases import get_access_token, register_product
-from products.models import Offer, Product
+from products.models import Product
 
 
-class TestCRUDProducts(TestCase):
+class TestProducts(TestCase):
     def setUp(self):
         self.client = APIClient()
 
@@ -37,20 +33,17 @@ class TestCRUDProducts(TestCase):
 
     def test_add_product(self):
         data = {
-            # "id": str(uuid.uuid4()),
             "name": "test product",
             "description": "test product description",
         }
         self.client.post("/api/products/", data, format="json")
 
         p = Product.objects.first()
-        # self.assertEqual(data["id"], str(p.id))
         self.assertEqual(data["name"], p.name)
         self.assertEqual(data["description"], p.description)
 
     def test_update_product(self):
         data = {
-            # "id": str(uuid.uuid4()),
             "name": "test product",
             "description": "test product description",
         }
@@ -72,21 +65,5 @@ class TestCRUDProducts(TestCase):
 
         self.assertEqual(Product.objects.count(), 0)
 
-    def test_register_product(self):
-        p = baker.make(Product)
-        register_product(p)
-
-    def test_offers(self):
-        id = "a0d60e80-59eb-412a-ab7f-06b929e92eb4"
-
-        url = f"https://python.exercise.applifting.cz/api/v1/products/{id}/offers"
-        resp = requests.get(
-            url,
-            headers={
-                "Bearer": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6ImIxZGY2MjdmLTFlMWUtNGMwYy1iZmNhLWEyYzA0NzZkMGE4NiIsImV4cGlyZXMiOjE2ODIzNDQ2OTR9.OkhErquMnsEAp6e_-iuGyqu7_Xz37hdXFbG48TZBYpE",
-                "Content-Type": "application/json",
-            },
-        )
-        # data = json.loads(resp.content)
-        print(resp.status_code)
-        print(resp.content)
+    def test_get_offers(self):
+        pass
